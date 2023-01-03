9525de96c4137e786960ecf586c7de8a6d153357
yajan-s <yajansingh08@gmail.com>
2023-01-03 16:48:28 -0500

    Fixed call button sizing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,15 @@ import callButton from '../assets/call.svg'
 
 const Navbar = () => {
     return (
-        <div className='flex z-50'>
-            <img src={logo} alt='CodeHex' className=' pl-20 pt-14'/>
+        <div className='flex z-50 pt-12 pr-20 pl-20'>
+            <img src={logo} alt='CodeHex' className=''/>
             <div className='grow flex invisible'></div>
-            <a href="tel:+16478943585">
-            <img className=' pt-12 pr-20 py-15' src={callButton} alt="CallUs"/>
-            </a>
+            <div className='flex flex-col'>
+                <a href="tel:+16478943585">
+                    <img className='' src={callButton} alt="CallUs"/>
+                </a>
+            <div className='grow flex invisible'/>
+            </div>
         </div>
     );
 }
